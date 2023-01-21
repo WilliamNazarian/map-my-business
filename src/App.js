f102464ab714559f31f9aaefd87884511d6bef78
Alec Tufenkjian <alectufenkjian@gmail.com>
2023-01-21 14:38:38 -0500

    Setup heatmap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627
+    },
+    zoom: 11
+  };
+
+  const heatMapData = {
+    positions: [
+      { lat: 55.5, lng: 34.56 },
+      { lat: 34.7, lng: 28.4 },
+    ],
+    options: {
+      radius: 20,
+      opacity: 0.6,
+    }
+  }
+
+  return(
+    // Important! Always set the container height explicitly
+    <div style = {{ height: '100vh', width: '100%' }} >
+    <GoogleMapReact
+      bootstrapURLKeys={{ key: "AIzaSyBhll2fXJ6qdqAJlBfBHv4g5y30vdM1IqY" }}
+      defaultCenter={defaultProps.center}
+      defaultZoom={defaultProps.zoom}
+      heatmapLibrary={true}
+      heatmap={heatMapData}
+    >
+      <AnyReactComponent
+        lat={59.955413}
+        lng={30.337844}
+        text="My Marker"
+      />
+    </GoogleMapReact>
+    </div >
   );
 }
 
