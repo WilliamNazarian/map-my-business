15533210524189148de4b3c3477550b6d111c6aa
egadb <egadboisroy@gmail.com>
2023-01-21 17:51:40 -0500

    discord test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
 import React from "react";
 import GoogleMapReact from 'google-map-react';
+import {Row, Col, Container, Button} from 'react-bootstrap';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const BOT_TOKEN = 'MTA2NjQ2Nzg5MzMyMTE1ODY5Ng.Gf2Kc0.7gL6mbWaTwI_Ni39VZCa7ZIL1bogPpd92vurRg';
+const CHANNEL_ID = '1066469784742527106';
+
+async function sendMessage(content) {
+  const response = await fetch(
+      `https://discord.com/api/v9/channels/${CHANNEL_ID}/messages`, {
+          method: 'post',
+          body: JSON.stringify({content}),
+          headers: {
+              'Authorization': `Bot ${BOT_TOKEN}`,
+              'Content-Type': 'application/json',
+          }
+      }
+  );
+  const data = await response.json();
+  return data;
+}
+
+async function getMessages() {
+  const response = await fetch(
+      `https://discord.com/api/v9/channels/${CHANNEL_ID}/messages`, {
+          method: 'get',
+          body: JSON.stringify({content}),
+          headers: {
+              'Authorization': `Bot ${BOT_TOKEN}`,
+              'Content-Type': 'application/json',
+          }
+      }
+  );
+  const data = await response.json();
+  return data;
+}
+
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -25,21 +61,29 @@ function App() {
 
   return(
     // Important! Always set the container height explicitly
-    <div style = {{ height: '100vh', width: '100%' }} >
-    <GoogleMapReact
-      bootstrapURLKeys={{ key: "AIzaSyBhll2fXJ6qdqAJlBfBHv4g5y30vdM1IqY" }}
-      defaultCenter={defaultProps.center}
-      defaultZoom={defaultProps.zoom}
-      heatmapLibrary={true}
-      heatmap={heatMapData}
-    >
-      <AnyReactComponent
-        lat={59.955413}
-        lng={30.337844}
-        text="My Marker"
-      />
-    </GoogleMapReact>
-    </div >
+    <Container style={{height: "100vh", maxWidth: "none" }}>
+      <Row style={{height: "100vh"}}>
+        <Col lg={8} style={{ padding: "0" }}>
+          <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyBhll2fXJ6qdqAJlBfBHv4g5y30vdM1IqY" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+        heatmapLibrary={true}
+        heatmap={heatMapData}
+          >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+          </GoogleMapReact>
+        </Col>
+        <Col lg={4}>
+          <Button onClick={() => getMessages()} variant="primary">sendMessage</Button>
+          <a> </a>
+        </Col>
+      </Row>
+    </Container >
   );
 }
 
